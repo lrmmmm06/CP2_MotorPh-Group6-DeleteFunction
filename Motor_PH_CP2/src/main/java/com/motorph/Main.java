66e51cad66aaa00fe54443cd46f0b8c7931b85f0
lrmmmm06 <lr.mmontanez@mmdc.mcl.edu.ph>
2026-06-21 09:05:46 +0800

    Add files via upload

---
+++ Motor_PH_CP2/src/main/java/com/motorph/Main.java
@@ -0,0 +1,27 @@
+package com.motorph;
+
+import com.motorph.gui.*;
+
+import javax.swing.*;
+import java.awt.*; 
+import java.awt.event.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    new MainFrame();
+    
+}
+} 
+
+
+/* public class Main { 
+ 
+ public static void main(String[] args) {
+
+ new MainFrame();   
+    
+ } 
+ }
+*/
